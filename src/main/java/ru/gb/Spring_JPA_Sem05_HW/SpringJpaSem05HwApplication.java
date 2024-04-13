ff990bab4195d91ac0c76000204f35d4eba0d8ab
alex123 <alex123@dot.com>
2024-04-13 08:53:16 +0500

    start

---
+++ src/main/java/ru/gb/Spring_JPA_Sem05_HW/SpringJpaSem05HwApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.Spring_JPA_Sem05_HW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaSem05HwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaSem05HwApplication.class, args);
+	}
+
+}
